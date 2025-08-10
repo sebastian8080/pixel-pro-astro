ebcb593c5270b09de2a66b37dccd13efd762d643
sebastian8080 <sebas25211@hotmail.com>
2025-08-10 16:15:16 -0500

    feat: add all services

--- src/data/services.ts
+++ src/data/services.ts
@@ -7,6 +7,7 @@ export interface Service {
     description: string;
     borderColor?: string; // Opcional, si quieres bordes de color diferentes
     details?: string[]; // Opcional, si quieres añadir una lista de detalles
+    url?: string;
   }
   
   export const services: Service[] = [
@@ -16,6 +17,7 @@ export interface Service {
       title: 'Gestión de redes sociales',
       description: 'Creación, publicación y monitoreo de contenido en plataformas como Instagram, Facebook y TikTok para aumentar la presencia de marca.',
       borderColor: 'border-purple-600',
+      url: 'gestion-redes-sociales'
     },
     {
       id: 'publicidad-google-ads',
@@ -23,6 +25,7 @@ export interface Service {
       title: 'Publicidad en Google Ads',
       description: 'Creación y optimización de campañas en Google para atraer clientes potenciales mediante anuncios de búsqueda, display y YouTube.',
       borderColor: 'border-blue-600',
+      url: 'publicidad-google-ads'
     },
     {
       id: 'publicidad-meta-ads',
@@ -30,6 +33,7 @@ export interface Service {
       title: 'Publicidad en Meta (Facebook e Instagram Ads)',
       description: 'Diseño y administración de campañas pagadas para alcanzar audiencias específicas y generar conversiones.',
       borderColor: 'border-pink-600',
+      url: 'publicidad-meta-ads'
     },
     {
       id: 'desarrollo-sitios-web',
@@ -37,6 +41,7 @@ export interface Service {
       title: 'Desarrollo de sitios web',
       description: 'Diseño y programación de sitios web atractivos, funcionales y adaptados a dispositivos móviles.',
       borderColor: 'border-green-600',
+      url: 'desarrollo-sitios-web'
     },
     {
       id: 'posicionamiento-seo',
@@ -44,6 +49,7 @@ export interface Service {
       title: 'Posicionamiento SEO',
       description: 'Optimización de contenido y estructura web para mejorar la visibilidad en los resultados orgánicos de Google.',
       borderColor: 'border-yellow-600',
+      url: 'posicionamiento-seo'
     },
     {
       id: 'diseno-grafico',
@@ -51,6 +57,7 @@ export interface Service {
       title: 'Diseño gráfico',
       description: 'Creación de piezas visuales como logos, banners, flyers y contenido para redes sociales, alineadas con la identidad de la marca.',
       borderColor: 'border-red-600',
+      url: 'diseno-grafico'
     },
     {
       id: 'branding-identidad-visual',
@@ -58,6 +65,7 @@ export interface Service {
       title: 'Branding e identidad visual',
       description: 'Desarrollo de la imagen corporativa, incluyendo logo, paleta de colores, tipografías y manual de marca.',
       borderColor: 'border-orange-600',
+      url: 'branding-e-identidad-visual'
     },
     {
       id: 'consultoria-marketing-digital',
@@ -65,5 +73,6 @@ export interface Service {
       title: 'Consultoría en marketing digital',
       description: 'Asesoramiento personalizado para definir estrategias digitales y optimizar resultados.',
       borderColor: 'border-teal-600',
+      url: 'consultoria-marketing-digital'
     },
 ];
